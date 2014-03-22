c6d65cf0ffb6f8b86c37ee8afd6ea578a3423ccb
Krzysiek <fistasheq+github@gmail.com>
2014-03-23 00:32:28 +0100

    editing works, but the listpage doesn't update; the rest works fine

---
+++ Affirmations/View/EditPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Affirmations.ViewModel;
+using Affirmations.Model;
+
+namespace Affirmations.View
+{
+    public partial class EditPage : PhoneApplicationPage
+    {
+        private string affirmationIndex;
+        private EditViewModel viewModel;
+
+        public EditPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            affirmationIndex = "-1";
+
+            if (NavigationContext.QueryString.TryGetValue("affirmationIndex", out affirmationIndex))
+            {
+                viewModel = new EditViewModel(affirmationIndex);
+                DataContext = viewModel;
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            Repository.Affirmations[Convert.ToInt16(affirmationIndex)].Text = tbText.Text;
+            Repository.SaveChanges();
+
+            try
+            {
+                NavigationService.GoBack();
+            }
+            catch (InvalidOperationException exception)
+            {
+                NavigationService.Navigate(new Uri("/View/ListPage.xaml", UriKind.Relative));
+            }
+
+        }
+    }
+}
---
+++ Affirmations/ViewModel/EditViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Affirmations.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Affirmations.ViewModel
+{
+    public class EditViewModel : BindableBase
+    {
+        public Affirmation EditedAffirmation { get; set; }
+
+        public EditViewModel(string affirmationIndex)
+        {
+            EditedAffirmation = Repository.Affirmations[Convert.ToInt16(affirmationIndex)];
+        }
+
+    }
+}
